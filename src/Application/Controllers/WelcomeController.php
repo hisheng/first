24819cbb84efc4c4fd588e32408c78b410fd2600
hisheng <465908774@qq.com>
2016-08-12 11:01:03 +0800

    增加 案列 post参数
    增加 案列 post参数

--- src/Application/Controllers/WelcomeController.php
+++ src/Application/Controllers/WelcomeController.php
@@ -10,7 +10,12 @@ class WelcomeController {
 
     // 支持 welcome/hello/3/4 形式的参数
     public function helloAction($hi = null,$h = null){
-        echo 'welcome hello'.$hi.$h;
+        echo 'welcome hello'.$hi.$h.BR;
+        $userid = $_POST['userid'];
+        $name = $_POST['name'];
+        $version = $_POST['version'];
+
+        echo $userid.'ss'.$name.'ss'.$version;
     }
 
 }
