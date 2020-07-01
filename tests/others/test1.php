d4b312b0596584148e3628b21c3758c99adc67a7
张海生 <zhanghaisheng@km.com>
2020-07-01 13:05:03 +0800

    最新

---
+++ tests/others/test1.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author 张海生<zhanghaisheng@km.com>
+ * @dateTime   : 2020/7/1 11:52
+ * @Desc
+ */
+
+if (4 > 5) {
+    var_dump(true);
+} else {
+    var_dump(false);
+}
+
+
+$a = (4 > 5) ? true : false;
+var_dump($a);
